c70e405e086c0ac1bfc556fd02ad724fb730ea01
Priyanshu Singh <shailsingh933@gamil.com>
2025-05-12 13:37:35 +0530

    first-commit

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+
+        <div>
+            <img src={assets.logo} className='mb-5 w-32' alt="" />
+            <p className='w-full md:w-2/3 text-gray-600'>
+            At BH Chart, we believe style has no limits. From elegant watches to trendy clothes, we bring the best for women, men, and kids. Dress for every moment, live every second in style. Fashion that fits every generation, watches that mark every memory — only at BH Chart.
+            </p>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>COMPANY</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>Home</li>
+                <li>About us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+            </ul>
+        </div>
+
+        <div>
+            <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+            <ul className='flex flex-col gap-1 text-gray-600'>
+                <li>+1-212-456-7890</li>
+                <li>contact@bhcart.com</li>
+            </ul>
+        </div>
+
+      </div>
+
+        <div>
+            <hr />
+            <p className='py-5 text-sm text-center'>Copyright 2025@ bh-chart.com - All Right Reserved.</p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Footer
